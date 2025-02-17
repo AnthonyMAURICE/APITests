1660ed4d85fe659ed1b627bc684609003900cb8f
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2025-02-17 16:14:10 +0100

    project creation

---
+++ API_Countries/Database/CountryDbContext.cs
@@ -0,0 +1,15 @@
+﻿using API_Countries.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Countries.Database
+{
+    public class CountryDbContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=db_countries;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ API_Countries/Models/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_Countries.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName ="char")]
+        [StringLength(2)]
+        public string CountryCode { get; set; }
+        
+        [Required]
+        public string CountryName { get; set; }
+    }
+}
