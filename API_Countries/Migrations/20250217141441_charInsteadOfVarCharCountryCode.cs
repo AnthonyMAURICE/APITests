aac0ade3242313939ab483048c4e69f8a9face13
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2025-02-17 15:50:42 +0100

    initial commit

---
+++ API_Countries/Migrations/20250217141441_charInsteadOfVarCharCountryCode.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Countries.Migrations
+{
+    /// <inheritdoc />
+    public partial class charInsteadOfVarCharCountryCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CountryCode",
+                table: "Countries",
+                type: "char(2)",
+                maxLength: 2,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2)",
+                oldMaxLength: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CountryCode",
+                table: "Countries",
+                type: "nvarchar(2)",
+                maxLength: 2,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "char(2)",
+                oldMaxLength: 2);
+        }
+    }
+}
